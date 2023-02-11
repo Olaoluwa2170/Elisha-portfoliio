36f3f10cca2c764105e996906045cda83e76070d
Babalola Elisha <elitebabson@gmail.com>
2023-02-11 12:54:28 +0100

    Simple Changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -7,8 +7,8 @@ import Portfolio from './Portfolio'
 const Layout = () => {
   return (
     <div><ul className="flex text-xs shadow-xs sm:shadow-lg w-5/6 ml-24 sm:ml-20 bg-secondary-100 rounded-full p-2 mt-3 sm:justify-center font-bold gap-5 sm:gap-20">
-    <li><img className='icons' src={about} alt="" /><a target={"_blank"} href={'https://github.com/Olaoluwa2170'}>About</a></li>
-    <li><img className='icons' src={port} alt="" /><a target={"_blank"} href={"https://twitter.com/elisha_babalola"}><Portfolio/></a></li>
+    <li><img className='icons' src={about} alt="" /><a target={"_blank"} href={'https://www.linkedin.com/in/elisha-babalola-python-dev/'}>About</a></li>
+    <li><img className='icons' src={port} alt="" /><a target={"_blank"} href={"https://docs.google.com/document/d/1d9aZNxf2143ZQTF8MBaDUvTjje7kK_jd4Cnx_2p6TI0/edit?usp=sharing"}>Portfolio</a></li>
     <li><img className='icons' src={project} alt="" /><a target={"_blank"} href={"https://github.com/Olaoluwa2170"}>Projects</a></li>
     <li><img className='icons' src={blog} alt="" /><a target={"_blank"} href={"https://elitebabson.blogspot.com"}>Blog/More</a></li>
 </ul></div>
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
+
+// const Portfolio = () => {
+//   const [downloading, setDownloading] = useState(false);
+
+//   const handleDownload = () => {
+//     setDownloading(true);
+//     const link = document.createElement('a');
+//     link.href = './assets/file/Elisha-Cv.pdf';
+//     link.download = './assets/file/Elisha-Cv.pdf';
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//     setDownloading(false);
+//   };
+
+//   return (
+//     <div className='inline'>
+//       <button className='' onClick={handleDownload} disabled={downloading}>
+//         {downloading ? 'Downloading...' : 'Download CV'}
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
+
+
+import React, { useState, useEffect } from 'react';
 
 const Portfolio = () => {
-  const [downloading, setDownloading] = useState(false);
+  const [pdfUrl, setPdfUrl] = useState(null);
 
-  const handleDownload = () => {
-    setDownloading(true);
-    const link = document.createElement('a');
-    link.href = '/path/to/your/pdf/file.pdf';
-    link.download = 'file.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    setDownloading(false);
-  };
+  useEffect(() => {
+    fetch('./assets/file/Elisha-Cv.pdf')
+      .then(res => res.blob())
+      .then(blob => {
+        setPdfUrl(URL.createObjectURL(blob));
+      });
+  }, []);
 
   return (
-    <div className='inline'>
-      <button className='' onClick={handleDownload} disabled={downloading}>
-        {downloading ? 'Downloading...' : 'Download CV'}
-      </button>
-    </div>
+    <a href={pdfUrl} download>
+      Download PDF
+    </a>
   );
 };
 
