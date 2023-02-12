cf93f842b554c1fde26c78b91c08dfee3f4732ae
Babalola Elisha <elitebabson@gmail.com>
2023-02-12 23:43:03 +0100

    corrected the reponsive problem with the nav bar and changed the icon and title

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ import project from './assets/img/project.svg'
 
 const Layout = () => {
   return (
-    <div><ul className="flex text-xs shadow-xs sm:shadow-lg sm:w-5/6 ml-24 sm:ml-20 bg-secondary-100 rounded-full p-2 mt-3 sm:justify-center font-bold gap-5 sm:gap-20">
+    <div><ul className="flex text-xs shadow-xs sm:shadow-lg sm:w-5/6 mr-2 pl-5 sm:pl-0 sm:ml-24 sm:ml-20 bg-secondary-100 rounded-full p-2 mt-3 sm:justify-center font-bold gap-5 sm:gap-20">
     <li><img className='icons' src={about} alt="" /><a target={"_blank"} href={'https://www.linkedin.com/in/elisha-babalola-python-dev/'}>About</a></li>
     <li><img className='icons' src={port} alt="" /><a target={"_blank"} href={"https://docs.google.com/document/d/1d9aZNxf2143ZQTF8MBaDUvTjje7kK_jd4Cnx_2p6TI0/edit?usp=sharing"}>Portfolio</a></li>
     <li><img className='icons' src={project} alt="" /><a target={"_blank"} href={"https://github.com/Olaoluwa2170"}>Projects</a></li>
